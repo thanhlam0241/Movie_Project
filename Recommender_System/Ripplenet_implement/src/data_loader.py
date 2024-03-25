ca308ffa2c90477449e8433defd0d4802c8c0318
Lam Nguyen <thienthan0241@gmail.com>
2024-03-25 16:30:42 +0000

    fix data loader, add requirement.txt

--- Recommender_System/Ripplenet_implement/src/data_loader.py
+++ Recommender_System/Ripplenet_implement/src/data_loader.py
@@ -2,9 +2,9 @@ import collections
 import os
 import numpy as np
 
-# 按照preprocess的处理结果得到两个文件kg_final.txt和rating_final.txt
-# KG的数据集格式是三元组：h    r   t
-# 推荐的评分数据集格式是：userid    itemid  rating
+# Theo kết quả xử lý của tiền xử lý, thu được hai tệp kg_final.txt và rating_final.txt.
+# Định dạng tập dữ liệu của KG là triple：h    r   t
+# Định dạng tập dữ liệu rating được đề xuất là：userid    itemid  rating
 
 
 def load_data(args):
@@ -17,7 +17,7 @@ def load_data(args):
 def load_rating(args):
     print('reading rating file ...')
 
-    # 读取评分数据集文件
+    # Đọc file tập dữ liệu rating
     rating_file = '../data/' + args.dataset + '/ratings_final'
     if os.path.exists(rating_file + '.npy'):
         rating_np = np.load(rating_file + '.npy')
@@ -33,19 +33,19 @@ def load_rating(args):
 def dataset_split(rating_np):
     print('splitting dataset ...')
 
-    # 分割比例：train:eval:test = 6:2:2
+    # chia tỷ lệ：train:eval:test = 6:2:2
     eval_ratio = 0.2
     test_ratio = 0.2
     n_ratings = rating_np.shape[0]
 
-    # 以分割比例随机选择切分数据集
+    # Chọn ngẫu nhiên tập dữ liệu
     eval_indices = np.random.choice(n_ratings, size=int(n_ratings * eval_ratio), replace=False)
     left = set(range(n_ratings)) - set(eval_indices)
     test_indices = np.random.choice(list(left), size=int(n_ratings * test_ratio), replace=False)
     train_indices = list(left - set(test_indices))
     # print(len(train_indices), len(eval_indices), len(test_indices))
 
-    # 遍历训练数据集，仅保留有历史评分的用户
+    # Duyệt qua tập dữ liệu train và chỉ giữ lại những người dùng có xếp hạng lịch sử
     user_history_dict = dict()
     for i in train_indices:
         user = rating_np[i][0]
@@ -55,7 +55,7 @@ def dataset_split(rating_np):
             if user not in user_history_dict:
                 user_history_dict[user] = []
             user_history_dict[user].append(item)
-            # 添加相应历史记录
+            # Thêm lịch sử tương ứng
 
     train_indices = [i for i in train_indices if rating_np[i][0] in user_history_dict]
     eval_indices = [i for i in eval_indices if rating_np[i][0] in user_history_dict]
@@ -72,7 +72,7 @@ def dataset_split(rating_np):
 def load_kg(args):
     print('reading KG file ...')
 
-    # 读取知识图谱文件
+    # Đọc tệp kg
     kg_file = '../data/' + args.dataset + '/kg_final'
     if os.path.exists(kg_file + '.npy'):
         kg_np = np.load(kg_file + '.npy')
@@ -81,12 +81,12 @@ def load_kg(args):
         np.save(kg_file + '.npy', kg_np)
 
     n_entity = len(set(kg_np[:, 0]) | set(kg_np[:, 2]))
-    # 头节点集与尾节点集取并，获得实体数量
+    # Hợp nhất tập hợp nút đầu và tập hợp nút đuôi để thu được số lượng thực thể
     n_relation = len(set(kg_np[:, 1]))
-    # 有向边集，获得关系数量
+    # Tập cạnh có hướng, lấy số lượng quan hệ
 
     kg = construct_kg(kg_np)
-    # 构建KG，实际上是构建了一个特殊字典
+    # Xây dựng KG thực sự xây dựng một từ điển đặc biệt
     return n_entity, n_relation, kg
 
 
@@ -95,32 +95,32 @@ def construct_kg(kg_np):
     kg = collections.defaultdict(list)
     # defaultdict(<class 'list'>, {head:list([(tail,relation),...])
     # defaultdict而非dict，注意二者区别：
-    # 读取defaultdict字典时，如未查中，则返回一个default，此处为list[]
+    # Khi đọc từ điển defaultdict, nếu không tìm thấy thì sẽ trả về giá trị mặc định, []
     for head, relation, tail in kg_np:
         kg[head].append((tail, relation))
-        # 具体构造，为头节点添加关系向量有向边以及尾节点
+        # Cấu trúc cụ thể, thêm các cạnh và nút đuôi của vectơ mối quan hệ vào nút đầu
     return kg
 
 
-# 构造ripple多跳结果集
+# Xây dựng tập kết quả multi-hop ripple
 def get_ripple_set(args, kg, user_history_dict):
     print('constructing ripple set ...')
 
     # user -> [(hop_0_heads, hop_0_relations, hop_0_tails), (hop_1_heads, hop_1_relations, hop_1_tails), ...]
     ripple_set = collections.defaultdict(list)
 
-    for user in user_history_dict:  # 遍历用户
-        for h in range(args.n_hop):  # 遍历多跳
+    for user in user_history_dict:  
+        for h in range(args.n_hop):  
             memories_h = []
             memories_r = []
             memories_t = []
 
-            if h == 0:  # 兴趣未传播，直接返回用户历史记录作为上一跳的兴趣
+            if h == 0:  # The interest is not propagated, and the user history is returned directly as the interest of the previous hop.
                 tails_of_last_hop = user_history_dict[user]
-            else:  # 兴趣进行传播
+            else:  # spread of interest
                 tails_of_last_hop = ripple_set[user][-1][2]
 
-            # 相应更新三元组特征
+            # Update triple features accordingly
             for entity in tails_of_last_hop:
                 for tail_and_relation in kg[entity]:
                     memories_h.append(entity)
@@ -132,8 +132,8 @@ def get_ripple_set(args, kg, user_history_dict):
             if len(memories_h) == 0:
                 ripple_set[user].append(ripple_set[user][-1])
             else:
-                # 为每个用户采样固定大小的邻居
-                # 采样是必须做出的妥协，考虑计算压力和噪声大小
+                # Sample fixed-size neighbors for each user
+                # Sampling is a compromise that must be made, taking into account computational pressure and noise levels
                 replace = len(memories_h) < args.n_memory
                 indices = np.random.choice(len(memories_h), size=args.n_memory, replace=replace)
                 memories_h = [memories_h[i] for i in indices]

3aa9fd22d970acb395c8f48d656186ccf428d904
Lam Nguyen <thienthan0241@gmail.com>
2024-05-09 14:55:28 +0000

    test ci

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ docker rm -v -f $(docker ps -qa)
 sleep 3
 docker rmi -f $(docker images -aq)
 sleep 3
+docker system prune --volumes
+sleep 3
 docker compose up -d --build
--- service/account_service/model/user.js
+++ service/account_service/model/user.js
@@ -1,35 +1,43 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
     id: {
-        type: Number,
-        required: true,
-        unique: true,
-        index: true
+      type: Number,
+      required: true,
+      unique: true,
+      index: true,
     },
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        index: true
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     avatar: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
-}, {
+  },
+  {
     toJSON: {
-        transform: function (doc, ret) {
-            delete ret._id;
-        }
-    }
-});
+      transform: function (doc, ret) {
+        delete ret._id;
+      },
+    },
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);

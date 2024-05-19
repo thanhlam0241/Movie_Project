b8600df7433e2872d9f4bb5d51fe3e82403ffe2b
Lam Nguyen <thienthan0241@gmail.com>
2024-05-19 16:43:16 +0000

    add exception

--- Ripplenet/src/main.py
+++ Ripplenet/src/main.py
@@ -3,34 +3,23 @@ from use import getRecommendOfUser
 import argparse
 from data_loader import load_data
 import numpy as np
+from config import get_param
 
 app = FastAPI()
 
-np.random.seed(555) 
-parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='movie', help='which dataset to use')
-parser.add_argument('--dim', type=int, default=16, help='dimension of entity and relation embeddings')
-parser.add_argument('--n_hop', type=int, default=2, help='maximum hops')
-parser.add_argument('--kge_weight', type=float, default=0.01, help='weight of the KGE term')
-parser.add_argument('--l2_weight', type=float, default=1e-7, help='weight of the l2 regularization term')
-parser.add_argument('--lr', type=float, default=0.02, help='learning rate')
-parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
-parser.add_argument('--n_epoch', type=int, default=10, help='the number of epochs')
-parser.add_argument('--n_memory', type=int, default=32, help='size of ripple set for each hop')
-parser.add_argument('--item_update_mode', type=str, default='plus_transform',
-                    help='how to update item at the end of each hop')
-parser.add_argument('--using_all_hops', type=bool, default=True,
-                    help='whether using outputs of all hops or just the last hop when making prediction')
-parser.add_argument('--model_dir', type=str, default='../model', help='directory to save model')
-parser.add_argument('--use_cuda', type=bool, default=False, help='whether to use gpu')
-parser.add_argument('--k', type=int, default=10, help='number of top_k')
+
 
 @app.get("/")
 def home(request: Request):
     return "Hello World"
 
-@app.get("/recommend")
-def recommend(request: Request):
-    args = parser.parse_args()
-    data_info = load_data(args) 
-    return getRecommendOfUser(self.args, self.data_info)
+@app.get("/recommend/{user_id}")
+def recommend(user_id: int):
+    args = get_param()
+    data_info = load_data(args)
+    return getRecommendOfUser(args, data_info, user_id)
+
+@app.exception_handler(Exception)
+async def validation_exception_handler(request, exc):
+    print(str(exc))
+    return PlainTextResponse("Something went wrong", status_code=400)
--- Ripplenet/src/use.py
+++ Ripplenet/src/use.py
@@ -1,6 +1,6 @@
 from predict import predict
 
-def getRecommendOfUser(args, data_info):
-    a, b = predict(args, data_info)
+def getRecommendOfUser(args, data_info, userId = None):
+    a, b = predict(args, data_info, userId)
     
-    return a, b
+    return b
